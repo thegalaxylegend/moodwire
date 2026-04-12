a6d327e7c12a0e3b667e89655dfea3c70b6b87c5
thegalaxylegend <thegalaxylegend@users.noreply.github.com>
2026-04-12 09:00:50 +0530

    fix(build): remove unused useNavigate import from TopicPage.tsx

--- src/pages/public/TopicPage.tsx
+++ src/pages/public/TopicPage.tsx
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import { useParams, Link, useNavigate } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import { SEO } from '../../components/SEO';
 import { Navbar } from '../../components/Navbar';
 import { Footer } from '../../components/Footer';
